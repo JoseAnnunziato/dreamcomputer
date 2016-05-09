5ebea9a3e9e035ca60647eebab4724cb9f7e70a4
Dreamcomputer <mallikarjun.s@husky.neu.edu>
2016-05-09 13:06:24 -0400

    Assignment 1

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.listen(port, ipaddress);
+
+
+
+/*
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -7,3 +18,4 @@ app.get('/hello', function(req, res){
     res.send('hello world');
 });
 app.listen(port, ipaddress);
+    */
